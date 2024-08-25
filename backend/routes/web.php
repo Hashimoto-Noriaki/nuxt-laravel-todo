7c497a91c88019887fff7cb404c8611c5d0f062f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-25 16:32:41 +0900

    Userのルーティングを設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TodosController;
+use App\Http\Controllers\Api\V1\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,4 +12,9 @@ Route::prefix('v1')->group(function () {
     Route::get('todos', [TodosController::class, 'index']);
     Route::post('todos', [TodosController::class, 'store']);
     Route::delete('todos/{id}', [TodosController::class, 'destroy']);
+});
+
+Route::prefix('v1')->group(function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('users', [UserController::class, 'store']);
 });
